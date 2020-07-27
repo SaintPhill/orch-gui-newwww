242d338feb999729710d5546807242282a49f800
Filipp <fboiko@iflex.ru>
2020-07-27 11:21:08 +0300

    add tricolor SvgIcon to Header

--- src/client/components/Header/HeaderTemplate.tsx
+++ src/client/components/Header/HeaderTemplate.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
 
 import './Header.scss';
+import { SvgIcon } from '../SvgIcon/';
 
 export function HeaderTemplate(): JSX.Element {
     const ROOT_CLASS = 'header';
 
     return (
         <header className={ROOT_CLASS}>
-            <div className={`${ROOT_CLASS}__icon`}>
-                Icon
-            </div>
+            <SvgIcon spriteId={'tricolor'} className={`${ROOT_CLASS}__icon`} />
             <div className={`${ROOT_CLASS}__wrapper`}>
                 <div className={`${ROOT_CLASS}__buttons-wrapper`}>
                     <div className={`${ROOT_CLASS}__button ${ROOT_CLASS}__button_active`}>
