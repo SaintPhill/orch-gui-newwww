36923b9609773ee1ebfd06f06fc4781d3d067faf
Filipp <fboiko@iflex.ru>
2020-07-27 12:11:42 +0300

    update ButtonTemplate

--- src/client/UI/button/ButtonTemplate.tsx
+++ src/client/UI/button/ButtonTemplate.tsx
@@ -1,27 +1,44 @@
-import React from 'react'
-import './button.scss'
+import React from 'react';
+import classNames from 'classnames';
 
-import classNames from 'classnames'
+import './button.scss';
+import { SvgIcon } from '../../components/SvgIcon';
 
-type Props = {
-    isPrimary: boolean,
-    children: string,
+interface Props {
+    svgId?: string
+    isPrimary?: boolean
+    className?: string
+    children?: React.ReactNode
+    onClick?: (
+        event?: React.MouseEvent<HTMLElement>
+        | React.FormEvent<HTMLFormElement>,
+    ) => void
 }
 
-export function ButtonTemplate({ isPrimary, children }: Props) {
+export function ButtonTemplate({
+    isPrimary,
+    children,
+    onClick,
+    svgId,
+    className,
+}: Props): JSX.Element {
     const btnClass = classNames(
         'button',
         {
-            'button_primary': isPrimary
+            'button_primary': isPrimary,
+            [String(className)]: !!className,
         }
-    )
+    );
 
     return (
-        <button
-            type='submit'
-            className={btnClass}
-        >
+        <button className={btnClass} onClick={onClick}>
             {children}
+            {svgId &&
+                <SvgIcon
+                    spriteId={svgId}
+                    className={`${className}-icon`}
+                />
+            }
         </button>
-    )
+    );
 }
