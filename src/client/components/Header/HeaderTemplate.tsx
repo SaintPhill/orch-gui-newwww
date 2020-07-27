d83d0a2032c0e909812f630ae5cfde35f12b79a9
Filipp <fboiko@iflex.ru>
2020-07-27 15:40:36 +0300

    add logout button

--- src/client/components/Header/HeaderTemplate.tsx
+++ src/client/components/Header/HeaderTemplate.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import './Header.scss';
 import { SvgIcon } from '../SvgIcon/';
+import { Button } from '../../UI/button';
 
 export function HeaderTemplate(): JSX.Element {
     const ROOT_CLASS = 'header';
@@ -22,6 +23,9 @@ export function HeaderTemplate(): JSX.Element {
                     <div className={`${ROOT_CLASS}__user-name`}>
                         Фамилияя Имя / Должность
                     </div>
+                    <Button svgId={'logout'} className={`${ROOT_CLASS}__logout-button`}>
+                        Выход
+                    </Button>
                 </div>
             </div>
         </header>
