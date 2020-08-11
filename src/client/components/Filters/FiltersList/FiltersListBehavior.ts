a837e23cc90e8f028e9c93b2a0a085f7c0536d0a
Filipp <fboiko@iflex.ru>
2020-08-11 10:45:24 +0300

    delete requestParams

--- src/client/components/Filters/FiltersList/FiltersListBehavior.ts
+++ src/client/components/Filters/FiltersList/FiltersListBehavior.ts
@@ -3,7 +3,6 @@ import React, { useEffect, useState } from 'react';
 import { FiltersListTemplate } from './FiltersListTemplate';
 import { SelectOption } from '../FilterItem/FiltersItem';
 import { GetFiltersValues } from '../../../apollo-query-types';
-import { RequestsParams } from '../../../../server/schemas/Requests/resolvers';
 import './FiltersList.scss';
 
 interface Props {
@@ -13,30 +12,8 @@ interface Props {
 export function FiltersListBehavior({
     filtersValuesQuery,
 }: Props): JSX.Element {
-    const [requestParams, setRequestParameter] = useState<RequestsParams>({
-        process: null,
-        status: null,
-        step: {
-            alias: null,
-            status: null,
-        },
-    });
-    console.log(requestParams);
-
     const [process, setProcess] = useState('');
     const [processStatus, setProcessStatus] = useState('');
-    useEffect(() => {
-        const processParameter = filtersValuesQuery?.processes?.find(queryProcess =>
-            queryProcess.name === process && queryProcess);
-        setRequestParameter(params => (
-            { ...params, process: processParameter ? processParameter.alias : null }
-        ));
-    }, [process, filtersValuesQuery]);
-    useEffect(() => {
-        setRequestParameter(params => (
-            { ...params, status: processStatus || null }
-        ));
-    }, [processStatus]);
 
     const [processSelectValues, setProcessSelectValues] = useState<SelectOption[]>();
     const [processStatusSelectValues, setProcessStatusSelectValues] = useState<SelectOption[]>();
